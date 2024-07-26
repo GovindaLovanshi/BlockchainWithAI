c2cbecd2dc69b781a9f03da77466070e1d0c09ca
Govinda Lovanshi <134533021+GovindaLovanshi@users.noreply.github.com>
2024-07-26 10:42:36 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,31 @@
-
+import Head from "next/head"
 import "../styles/globals.css";
+
+import { StateContextProvider } from "../Context";
 
 
 export default function App({ Component, pageProps }) {
   return (
-        <Component {...pageProps} />
+    <>
+     <Head>
+       <link ref="icon" href="../public/assets/images/favicon.png"/>
+     </Head>
+
+     <StateContextProvider>
+     <Component {...pageProps} />
+     </StateContextProvider>
+
+     <script src="../public/assets/js/bootstrap.bundle.min.js"></script>
+     <script src="../public/assets/js/swiper-bundle.min.js"></script>
+     <script src="../public/assets/js/aos.js"></script>
+     <script src="../public/assets/js/custom-aos.js"></script>
+     <script src="../public/assets/js/home-animation.js"></script>
+     <script src="../public/assets/js/header_sticky.js"></script>
+     <script src="../public/assets/js/script.js"></script>
+     <script src="../public/assets/js/bootstrap.bundle.min.js"></script>
+    
+     
+    </>
+        
   );
 }
